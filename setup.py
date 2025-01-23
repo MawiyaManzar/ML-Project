1fcac315e4c46363bc6395adc02a1eecc5f79a15
MawiyaManzar <mawiyamanzar28@gmail.com>
2025-01-23 10:01:48 +0530

    Add Dockerfile and update setup.py; create home.html for student exam performance prediction

--- setup.py
+++ setup.py
@@ -1,28 +1,31 @@
-from setuptools import find_packages,setup
+from setuptools import find_packages, setup
 from typing import List
 
-hyphen='-e .'
-def get_requirements(file_path:str)->List[str]:
-    '''     
-    this function will return the list of requirements
-    '''
-    requirements=[]
+# Define a string that will be removed from the requirements list if present
+hyphen = '-e .'
+
+def get_requirements(file_path: str) -> List[str]:
+    """Return the list of requirements.
+
+    This function reads the requirements from the given file path and returns them as a list.
+    """
+    requirements = []
+    # Open the requirements file and read its contents
     with open(file_path) as file_obj:
-        requirements=file_obj.readlines()
-        requirements=[req.replace("\n","")for req in requirements]
+        # Read each line in the file, remove newline characters, and add to the list
+        requirements = [req.replace("\n", "") for req in file_obj.readlines()]
 
-
-
+        # Remove the hyphen string if it is present in the list
         if hyphen in requirements:
             requirements.remove(hyphen)
-    return requirements        
-setup( 
+    return requirements
 
-    name='mlproject',
-    version='0.0.1',
-    author="mawiya",
-    author_email='mawiyamanzar@gmail.com',
-    packages=find_packages(),
-    install_requires=get_requirements('requirements.txt')
-
+# Define the setup configuration for the package
+setup(
+    name='mlproject',  # Name of the package
+    version='0.0.1',  # Version of the package
+    author="mawiya",  # Author of the package
+    author_email='mawiyamanzar@gmail.com',  # Author's email
+    packages=find_packages(),  # Automatically find packages in the directory
+    install_requires=get_requirements('requirements.txt')  # List of dependencies
 )
